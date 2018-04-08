fb81190fb7c79e7499dd02b2755813fc31edb697
zhuanxuhit <893051481@qq.com>
2018-04-08 17:10:21 +0800

    新增xgboost相关调试

---
+++ avazu-ctr-prediction/meanencoder.py
@@ -0,0 +1,133 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold
+from itertools import product
+
+class MeanEncoder:
+    def __init__(self, categorical_features, n_splits=5, target_type='classification', prior_weight_func=None):
+        """
+        :param categorical_features: list of str, the name of the categorical columns to encode
+
+        :param n_splits: the number of splits used in mean encoding
+
+        :param target_type: str, 'regression' or 'classification'
+
+        :param prior_weight_func:
+        a function that takes in the number of observations, and outputs prior weight
+        when a dict is passed, the default exponential decay function will be used:
+        k: the number of observations needed for the posterior to be weighted equally as the prior
+        f: larger f --> smaller slope
+        """
+
+        self.categorical_features = categorical_features
+        self.n_splits = n_splits
+        self.learned_stats = {}
+
+        if target_type == 'classification':
+            self.target_type = target_type
+            self.target_values = []
+        else:
+            self.target_type = 'regression'
+            self.target_values = None
+
+        if isinstance(prior_weight_func, dict):
+            self.prior_weight_func = eval('lambda x: 1 / (1 + np.exp((x - k) / f))', dict(prior_weight_func, np=np))
+        elif callable(prior_weight_func):
+            self.prior_weight_func = prior_weight_func
+        else:
+            self.prior_weight_func = lambda x: 1 / (1 + np.exp((x - 2) / 1))
+            
+    @staticmethod
+    def mean_encode_subroutine(X_train, y_train, X_test, variable, target, prior_weight_func):
+#         X_train = X_train[[variable]].copy()
+#         X_test = X_test[[variable]].copy()
+        X_train = X_train[[variable]]
+         X_test = X_test[[variable]]
+
+        if target is not None:
+            nf_name = '{}_pred_{}'.format(variable, target)
+            X_train['pred_temp'] = (y_train == target).astype(int)  # classification
+        else:
+            nf_name = '{}_pred'.format(variable)
+            X_train['pred_temp'] = y_train  # regression
+        ## 先验概率
+        prior = X_train['pred_temp'].mean()
+        
+        col_avg_y = X_train.groupby(by=variable, axis=0)['pred_temp'].agg({'mean': 'mean', 'beta': 'size'})
+        del X_train['pred_temp']
+        col_avg_y['beta'] = prior_weight_func(col_avg_y['beta'])
+        # 先验 + 后验
+        col_avg_y[nf_name] = col_avg_y['beta'] * prior + (1 - col_avg_y['beta']) * col_avg_y['mean']
+        col_avg_y.drop(['beta', 'mean'], axis=1, inplace=True)
+        # 取出了train中每个值的编码
+        nf_train = X_train.join(col_avg_y, on=variable)[nf_name].values
+        # 如果test中出现了没有特征类别，则使用先验概率填充
+        nf_test = X_test.join(col_avg_y, on=variable).fillna(prior, inplace=False)[nf_name].values
+
+        return nf_train, nf_test, prior, col_avg_y
+    def fit_transform(self, X, y):
+        """
+        :param X: pandas DataFrame, n_samples * n_features
+        :param y: pandas Series or numpy array, n_samples
+        :return X_new: the transformed pandas DataFrame containing mean-encoded categorical features
+        """
+        # 注意，不进行copy，太消耗内存了，我们在原先输入的基础上就行修改
+#         X_new = X.copy()
+        X_new = X
+        if self.target_type == 'classification':
+            skf = StratifiedKFold(self.n_splits)
+        else:
+            skf = KFold(self.n_splits)
+
+        if self.target_type == 'classification':
+            self.target_values = sorted(set(y))
+            self.learned_stats = {'{}_pred_{}'.format(variable, target): [] for variable, target in
+                                  product(self.categorical_features, self.target_values)}
+            for variable, target in product(self.categorical_features, self.target_values):
+                nf_name = '{}_pred_{}'.format(variable, target)
+                X_new.loc[:, nf_name] = np.nan
+                for large_ind, small_ind in skf.split(y, y):
+                    nf_large, nf_small, prior, col_avg_y = MeanEncoder.mean_encode_subroutine(
+                        X_new.iloc[large_ind], y.iloc[large_ind], X_new.iloc[small_ind], variable, target, self.prior_weight_func)
+                    
+                    X_new.iloc[small_ind, -1] = nf_small
+                    self.learned_stats[nf_name].append((prior, col_avg_y))
+        else:
+            self.learned_stats = {'{}_pred'.format(variable): [] for variable in self.categorical_features}
+            for variable in self.categorical_features:
+                nf_name = '{}_pred'.format(variable)
+                X_new.loc[:, nf_name] = np.nan
+                for large_ind, small_ind in skf.split(y, y):
+                    nf_large, nf_small, prior, col_avg_y = MeanEncoder.mean_encode_subroutine(
+                        X_new.iloc[large_ind], y.iloc[large_ind], X_new.iloc[small_ind], variable, None, self.prior_weight_func)
+                    X_new.iloc[small_ind, -1] = nf_small
+                    self.learned_stats[nf_name].append((prior, col_avg_y))
+        return X_new
+
+    def transform(self, X):
+        """
+        :param X: pandas DataFrame, n_samples * n_features
+        :return X_new: the transformed pandas DataFrame containing mean-encoded categorical features
+        """
+        # 注意，不进行copy，太消耗内存了，我们在原先输入的基础上就行修改
+#         X_new = X.copy()
+        X_new = X
+
+        if self.target_type == 'classification':
+            for variable, target in product(self.categorical_features, self.target_values):
+                nf_name = '{}_pred_{}'.format(variable, target)
+                X_new[nf_name] = 0
+                for prior, col_avg_y in self.learned_stats[nf_name]:
+                    X_new[nf_name] += X_new[[variable]].join(col_avg_y, on=variable).fillna(prior, inplace=False)[
+                        nf_name]
+                X_new[nf_name] /= self.n_splits
+        else:
+            for variable in self.categorical_features:
+                nf_name = '{}_pred'.format(variable)
+                X_new[nf_name] = 0
+                for prior, col_avg_y in self.learned_stats[nf_name]:
+                    X_new[nf_name] += X_new[[variable]].join(col_avg_y, on=variable).fillna(prior, inplace=False)[
+                        nf_name]
+                X_new[nf_name] /= self.n_splits
+
+        return X_new
